2c5fee94b17d5ad5bbccb5a6950c29fe02fb0a7f
tiagoruivo10 <tiagosantarim2017@gmail.com>
2026-04-10 18:20:49 -0300

    fazendo algumas atualizações para deixar mais dinâmico.

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -1,38 +1,86 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
-import { FcEmptyTrash, FcCheckmark } from "react-icons/fc";
 
-import { Container, ToDoList, Input, Button, ListItem } from "./styles";
+import {
+  Container,
+  ToDoList,
+  Input,
+  Button,
+  ListItem,
+  Check,
+  Trash,
+} from "./styles";
 
 function App() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(() => {
+    const tarefasSalvas = localStorage.getItem("@lista-tarefas");
+
+    if (tarefasSalvas) {
+      return JSON.parse(tarefasSalvas);
+    }
+
+    return [];
+  });
+
   const [inputTask, setInputTask] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("@lista-tarefas", JSON.stringify(list));
+  }, [list]);
 
   function inputMudou(event) {
     setInputTask(event.target.value);
   }
 
   function cliqueiNoBotao() {
-    setList([...list, { id: uuid(), task: inputTask }]);
+    if (inputTask.trim()) {
+      setList([...list, { id: uuid(), task: inputTask, finished: false }]);
+      setInputTask(""); //
+    }
+  }
+
+  function finalizarTarefa(id) {
+    const newList = list.map((item) =>
+      item.id === id ? { ...item, finished: !item.finished } : item,
+    );
+
+    setList(newList);
+  }
+
+  function removerTarefa(id) {
+    const newList = list.filter((item) => item.id !== id);
+
+    setList(newList);
   }
 
   return (
     <Container>
+      <h1>Lista de tarefas</h1>
+
       <ToDoList>
-        <Input onChange={inputMudou} placeholder="O que tenho para fazer" />
+        <Input
+          onChange={inputMudou}
+          value={inputTask}
+          placeholder="O que tenho para fazer"
+        />
         <Button onClick={cliqueiNoBotao}> Adicionar </Button>
 
         <ul>
-          {list.map((item) => (
-            <ListItem>
-              <FcCheckmark />
-              <li key={item.id}>{item.task}</li>
-              <FcEmptyTrash />
-            </ListItem> 
-          ))}
+          {list.length > 0 ? (
+            list.map((item) => (
+              <ListItem isFinished={item.finished} key={item.id}>
+                <Check onClick={() => finalizarTarefa(item.id)} />
+                <li>{item.task}</li>
+                <Trash onClick={() => removerTarefa(item.id)} />
+              </ListItem>
+            ))
+          ) : (
+            <h3>Não há tarefas.</h3>
+          )}
         </ul>
       </ToDoList>
     </Container>
   );
 }
+
 export default App;
--- projeto-react/src/styles.js
+++ projeto-react/src/styles.js
@@ -1,22 +1,37 @@
 import styled from "styled-components";
 
+import { FcEmptyTrash, FcCheckmark } from "react-icons/fc";
+
 export const Container = styled.div`
   background: linear-gradient(90deg, #383838 0%, #000000 81.25%);
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  h1 {
+    color: #ffffff;
+    margin-bottom: 30px;
+    font-size: 36px;
+  }
 `;
 
 export const ToDoList = styled.div`
   background: white;
   padding: 30px 20px;
   border-radius: 5px;
+  max-height: 400px;
+  overflow-y: auto;
 
   ul {
     padding: 0;
     margin-top: 60px;
+
+    h3 {
+      text-align: center;
+    }
   }
 `;
 
@@ -38,10 +53,19 @@ export const Button = styled.button`
   border: none;
   color: #fff;
   width: 130px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
 `;
 
 export const ListItem = styled.div`
-  background: #e4e4e4;
+  background: ${(props) => (props.isFinished ? "#04f45c" : "#E4E4E4")};
   box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
   border-radius: 5px;
   height: 60px;
@@ -55,4 +79,12 @@ export const ListItem = styled.div`
   li {
     list-style: none;
   }
+`;
+
+export const Trash = styled(FcEmptyTrash)`
+  cursor: pointer;
+`;
+
+export const Check = styled(FcCheckmark)`
+  cursor: pointer;
 `;
