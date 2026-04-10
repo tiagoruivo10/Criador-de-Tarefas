578b9f7bd0b05d873ccbcdc5224e6ac0571185bd
tiagoruivo10 <tiagosantarim2017@gmail.com>
2026-04-10 13:33:08 -0300

    finalizando a estilização da aplicação.

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
+import { FcEmptyTrash, FcCheckmark } from "react-icons/fc";
+
+import { Container, ToDoList, Input, Button, ListItem } from "./styles";
 
 function App() {
   const [list, setList] = useState([]);
@@ -14,16 +17,22 @@ function App() {
   }
 
   return (
-    <div>
-      <input onChange={inputMudou} placeholder="O que tenho para fazer" />
-      <button onClick={cliqueiNoBotao}> Adicionar </button>
+    <Container>
+      <ToDoList>
+        <Input onChange={inputMudou} placeholder="O que tenho para fazer" />
+        <Button onClick={cliqueiNoBotao}> Adicionar </Button>
 
-      <ul>
-        {list.map((item) => (
-          <li key={item.id}>{item.task}</li>
-        ))}
-      </ul>
-    </div>
+        <ul>
+          {list.map((item) => (
+            <ListItem>
+              <FcCheckmark />
+              <li key={item.id}>{item.task}</li>
+              <FcEmptyTrash />
+            </ListItem> 
+          ))}
+        </ul>
+      </ToDoList>
+    </Container>
   );
 }
 export default App;
